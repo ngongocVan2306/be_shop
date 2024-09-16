ad976e48b1b1feac0e1d3250e11065e04350ec6a
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-09-16 14:22:46 +0700

    update

--- src/configs/connectDB.ts
+++ src/configs/connectDB.ts
@@ -1,30 +1,8 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('shop', 'root', undefined, {
-    dialect: 'mysql',
-    host: 'localhost',
-    define: {
-        charset: 'utf8',
-        collate: 'utf8_general_ci',
-        timestamps: true,
-    },
-    logging: false,
-});
-
-export default async function connectDB() {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
-
-// import { Sequelize } from 'sequelize';
-
-// export const sequelize = new Sequelize('bkhyolwqd76qiibljtzz', 'utqeiigwqkav7zwd', 'm2EbljFmmh4PjxHtD0Zq', {
+// export const sequelize = new Sequelize('shop', 'root', undefined, {
 //     dialect: 'mysql',
-//     host: 'bkhyolwqd76qiibljtzz-mysql.services.clever-cloud.com',
+//     host: 'localhost',
 //     define: {
 //         charset: 'utf8',
 //         collate: 'utf8_general_ci',
@@ -41,3 +19,23 @@ export default async function connectDB() {
 //         console.error('Unable to connect to the database:', error);
 //     }
 // }
+
+export const sequelize = new Sequelize('bkhyolwqd76qiibljtzz', 'utqeiigwqkav7zwd', 'm2EbljFmmh4PjxHtD0Zq', {
+    dialect: 'mysql',
+    host: 'bkhyolwqd76qiibljtzz-mysql.services.clever-cloud.com',
+    define: {
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+        timestamps: true,
+    },
+    logging: false,
+});
+
+export default async function connectDB() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
